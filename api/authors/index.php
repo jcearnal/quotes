e4e9b48ad38beba5a387c771867a9eba6fff6db6
James Cearnal <james.cearnal@mail.fhsu.edu>
2024-03-10 15:34:50 -0400

    Attempting to PUT/POST in authors

--- api/authors/index.php
+++ api/authors/index.php
@@ -59,7 +59,7 @@ switch ($method) {
         include 'delete.php';
         break;
     default:
-        http_response_code(405); // Method Not Allowed
+        //http_response_code(405); // Method Not Allowed
         echo json_encode(['message' => 'Method Not Allowed']);
         break;
 }
